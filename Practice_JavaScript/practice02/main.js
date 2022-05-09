932b549fdbca9cfe2c92f2b76fef90887360e2a2
subaba <subaba199@gmail.com>
2022-05-10 05:29:05 +0900

    Chapter09: step01 -> 04

---
+++ Practice_JavaScript/practice02/main.js
@@ -0,0 +1,32 @@
+alert("↓コンソールをチェック！");
+
+const lastName  = "田中";
+const firstName = "一郎";
+const fullName  = lastName + " " + firstName;
+
+console.log( "lastName? : ", lastName );
+console.log( "firstName? : ", firstName );
+console.log( "fullName? : ", fullName );
+console.log( "======================" );
+
+const myAge        = 20;
+const friendAge    = 23;
+const totalAge     = myAge + friendAge;
+const numberOfDays = totalAge * 365;
+
+console.log( "myAge? : ", myAge );
+console.log( "friendAge? : ", friendAge );
+console.log( "totalAge? : ", totalAge );
+console.log( "numberOfDays? : ", numberOfDays );
+console.log( "======================" );
+
+const numberOfChildren     = 3;
+const numberOfCakes        = 10;
+const numberOfCakesByChild = numberOfCakes / numberOfChildren;
+const numberOfRestOfCakes  = numberOfCakes % numberOfChildren;
+
+console.log( "numberOfChildren? : ", numberOfChildren );
+console.log( "numberOfCakes? : ", numberOfCakes );
+console.log( "numberOfCakesByChild? : ", numberOfCakesByChild );
+console.log( "numberOfRestOfCakes? : ", numberOfRestOfCakes );
+console.log( "======================" );
